587985c48bce81c89b9bcb8637988f84ac4dd38b
Jonathan Tuzman <jonathan.tuzman@butcherbox.com>
2024-04-19 15:12:08 -0400

    util functions in test file

--- app/data/Schedule.test.ts
+++ app/data/Schedule.test.ts
@@ -1,21 +1,33 @@
 import CalendarGig from "~/data/CalendarGig";
 import EmailGig from "~/data/EmailGig";
 import Schedule from "~/data/Schedule";
+function to2Digits(num: number) {
+  return num.toString().padStart(2, "0")
+}
+
+function makeStartAndEndStrings(day: number) {
+  const start = `2024-12-${to2Digits(day)}T19:00:00-04:00`;
+  const end = `2024-12-${to2Digits(day)}T23:00:00-04:00`;
+  return [start, end];
+}
 
 describe("Schedule", () => {
   describe("Schedule.build", () => {
     it("builds an event set when events match", () => {
-      const start = "2024-12-01T19:00:00-04:00";
-      const end = "2024-12-01T23:00:00-04:00";
+      const [start, end] = makeStartAndEndStrings(1)
       const sched = Schedule.build({
         emailGigs: [EmailGig.make("somewhere", start, end)],
         calendarGigs: [CalendarGig.make("somewhere", start, end)]
       });
 
       const sets = sched.getEventSets();
-      expect(sets).toHaveLength(1)
-      expect(sets[0].emailGig.getId()).toEqual('2024-12-01')
-      expect(sets[0].calendarGig.getId()).toEqual('2024-12-01')
+      expect(sets).toHaveLength(1);
+      expect(sets[0].emailGig.getId()).toEqual("2024-12-01");
+      expect(sets[0].calendarGig.getId()).toEqual("2024-12-01");
+    });
+
+    it("creates a calendar event for a new email event", () => {
+
     });
   });
 });
