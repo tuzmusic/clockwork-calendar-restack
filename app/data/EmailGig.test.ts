0944dcbf02af0f9124da3fe9b370f49171b4d962
Jonathan Tuzman <jonathan.tuzman@butcherbox.com>
2024-05-01 09:57:56 -0400

    use test.extend for EmailGig.makeFullCalendarGig

--- app/data/EmailGig.test.ts
+++ app/data/EmailGig.test.ts
@@ -1,5 +1,11 @@
 import EmailGig from "~/data/EmailGig";
+import FullCalendarGig from "~/data/FullCalendarGig";
 import { getDistanceServiceWithMocks, testBasicGigInfo } from "~/data/testUtils";
+
+const start = "2024-12-01T19:00:00-04:00";
+const end = "2024-12-01T23:00:00-04:00";
+const location = "somewhere";
+
 
 describe("EmailGig", () => {
   describe("EmailGig.make", () => {
@@ -9,12 +15,27 @@ describe("EmailGig", () => {
   });
 
   describe("EmailGig.makeFullCalendarGig", () => {
-    testBasicGigInfo(
-      (location, start, end) =>
-        EmailGig.makeFullCalendarGig(
+    const distanceService = getDistanceServiceWithMocks(location);
+
+    afterEach(() => {
+      distanceService.getDistanceInfo.mockReset();
+    });
+
+    const it = test.extend<{ gig: FullCalendarGig }>({
+      gig: async ({ task: _ }, use) => {
+        const gig = await EmailGig.makeFullCalendarGig(
           EmailGig.make(location, start, end),
-          getDistanceServiceWithMocks(location)
-        )
-    );
+          distanceService
+        );
+        return await use(gig);
+      }
+    });
+
+    it("creates the basic gig", async ({ gig }) => {
+      expect(gig.getLocation()).toEqual(location);
+      expect(gig.getStartTime().dateTime).toEqual(start);
+      expect(gig.getEndTime().dateTime).toEqual(end);
+      expect(gig.getId()).toEqual("2024-12-01");
+    });
   });
 });
