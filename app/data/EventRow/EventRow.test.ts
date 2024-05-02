17957a0ad6ef757532118c38e1bf096b05d779e1
Jonathan Tuzman <jonathan.tuzman@butcherbox.com>
2024-05-02 12:40:54 -0400

    first failing test for EventRow

---
+++ app/data/EventRow/EventRow.test.ts
@@ -0,0 +1,29 @@
+import { calendar_v3 } from "googleapis";
+import { mock } from "vitest-mock-extended";
+
+import EmailGig from "~/data/EmailGig";
+import GoogleGig from "~/data/EventRow/GoogleGig";
+
+const location = "wherever";
+const start = "2024-12-01T19:00:00-04:00";
+const end = "2024-12-01T23:00:00-04:00";
+
+describe("EventRow", () => {
+  describe("EventRow.buildRow", () => {
+    describe("Email gig, incomplete calendar gig", () => {
+      describe("basic info matches", () => {
+
+        const emailGig = EmailGig.make(location, start, end);
+        const simpleCalendarGig = GoogleGig.make(
+          mock<calendar_v3.Schema$Event>({
+            start: { dateTime: start },
+            end: { dateTime: end },
+            location
+          })
+        );
+
+        const row = EventRow.buildRow(emailGig, simpleCalendarGig)
+      });
+    });
+  });
+});
