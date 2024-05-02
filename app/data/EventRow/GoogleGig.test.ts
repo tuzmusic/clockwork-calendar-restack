173af0360e5c79a01257e18524dabc9723ebcc00
Jonathan Tuzman <jonathan.tuzman@butcherbox.com>
2024-05-02 13:37:36 -0400

    put remote distance info into GoogleGig

--- app/data/EventRow/GoogleGig.test.ts
+++ app/data/EventRow/GoogleGig.test.ts
@@ -2,6 +2,7 @@ import { calendar_v3 } from "googleapis";
 import { mock } from "vitest-mock-extended";
 
 import GoogleGig from "~/data/EventRow/GoogleGig";
+import { DistanceData } from "~/data/types";
 
 const location = "wherever";
 const start = "2024-12-01T19:00:00-04:00";
@@ -15,7 +16,7 @@ describe("GoogleGig.make", () => {
       location
     });
 
-    const gig = GoogleGig.make(mockData)
+    const gig = GoogleGig.make(mockData);
 
 
     it("returns a simple gig", () => {
@@ -26,8 +27,35 @@ describe("GoogleGig.make", () => {
     });
 
     it("has the extendedProperties as null", () => {
-      expect(gig.getRouteInfo()).toBeNull()
-      expect(gig.getEventParts()).toBeNull()
+      expect(gig.getRouteInfo()).toBeNull();
+      expect(gig.getEventParts()).toBeNull();
+    });
+  });
+
+  describe("with route info", () => {
+    const mockDistanceData = {
+      fromHome: { miles: 1, minutes: 10, formattedTime: "10m" },
+      withWaltham: { miles: 2, minutes: 20, formattedTime: "20m" },
+      walthamDetour: { miles: 3, minutes: 30, formattedTime: "30m" },
+      fromWaltham: { miles: 4, minutes: 40, formattedTime: "40m" },
+      fromBoston: { miles: 5, minutes: 50, formattedTime: "0mm" }
+    } satisfies Record<string, DistanceData>;
+
+    const mockData = mock<calendar_v3.Schema$Event>({
+      start: { dateTime: start },
+      end: { dateTime: end },
+      location,
+      extendedProperties: {
+        private: {
+          distanceInfo: JSON.stringify(mockDistanceData)
+        }
+      }
+    });
+
+    const gig = GoogleGig.make(mockData);
+
+    it("populates the route info from the stored gig", () => {
+      expect(gig.getRouteInfo()).toEqual(mockDistanceData);
     });
   });
 });
--- app/data/EventRow/GoogleGig.ts
+++ app/data/EventRow/GoogleGig.ts
@@ -1,10 +1,13 @@
 import { calendar_v3 } from "googleapis";
 
 import Gig from "~/data/Gig";
+import { DistanceData } from "~/data/types";
 
 export default class GoogleGig extends Gig {
+  private routeInfo: Record<string, DistanceData> | null = null
+
   public getRouteInfo() {
-    return null
+    return this.routeInfo
   }
 
   public getEventParts() {
@@ -12,10 +15,18 @@ export default class GoogleGig extends Gig {
   }
 
   static make(json: calendar_v3.Schema$Event) {
-    return new GoogleGig(
+    const gig = new GoogleGig(
       json.location!,
       json.start!.dateTime!,
       json.end!.dateTime!,
     )
+
+    const extendedProps = json.extendedProperties?.private;
+
+    if (extendedProps) {
+      gig.routeInfo = JSON.parse(extendedProps.distanceInfo ?? null)
+    }
+
+    return gig
   }
 }
