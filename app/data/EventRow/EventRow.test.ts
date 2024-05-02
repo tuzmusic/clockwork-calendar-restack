63864318fd1e5d76140dec1586be727ff100743b
Jonathan Tuzman <jonathan.tuzman@butcherbox.com>
2024-05-02 17:23:56 -0400

    failing tests for full calendar gig with changed location

--- app/data/EventRow/EventRow.test.ts
+++ app/data/EventRow/EventRow.test.ts
@@ -120,6 +120,103 @@ describe("EventRow", () => {
         });
       });
 
+      describe("Email gig + Basic Calendar Gig; Basic info differs between the email and calendar gig", () => {
+        const updatedLocation = 'somewhere else'
+        const it = test.extend<{ row: EventRow }>({
+          row: async ({ task: _ }, use) => {
+            const mockData = mock<calendar_v3.Schema$Event>({
+              start: { dateTime: start },
+              end: { dateTime: end },
+              location
+            });
+            const emailGig = EmailGig.make(updatedLocation, start, end);
+            const calendarGig = GoogleGig.make(mockData);
+
+            const row = EventRow.buildRow(emailGig, calendarGig, distanceService);
+            expect(row).instanceof(EventRow);
+            return await use(row);
+          }
+        });
+
+        it("is a FullCalendarGig", ({ row: { appGig } }) => {
+          expect(appGig).instanceof(FullCalendarGig);
+        });
+
+        it("has basic info that matches the email gig", ({ row: { appGig } }) => {
+          expect(appGig.getLocation()).toEqual(updatedLocation);
+          expect(appGig.getStartTime().dateTime).toEqual(start);
+          expect(appGig.getEndTime().dateTime).toEqual(end);
+          expect(appGig.getId()).toEqual("2024-12-01");
+        });
+
+        it("starts with no route info", ({ row: { appGig } }) => {
+          expect(appGig.getRouteInfo()).toBeNull();
+        });
+
+        it("can fill out the route info on demand", async ({ row: { appGig } }) => {
+          expect(distanceService.getDistanceInfo).not.toHaveBeenCalled();
+          await appGig.fetchRouteInfo();
+          expect(distanceService.getDistanceInfo).toHaveBeenCalled();
+          expect(appGig.getRouteInfo()).not.toBeNull();
+        });
+      });
+
+      describe("Email gig + Full Calendar Gig; Location differs between the email and calendar gig", () => {
+        const updatedLocation = 'somewher else'
+
+        const mockDistanceData = {
+          fromHome: { miles: 1, minutes: 10, formattedTime: "10m" },
+          withWaltham: { miles: 2, minutes: 20, formattedTime: "20m" },
+          walthamDetour: { miles: 3, minutes: 30, formattedTime: "30m" },
+          fromWaltham: { miles: 4, minutes: 40, formattedTime: "40m" },
+          fromBoston: { miles: 5, minutes: 50, formattedTime: "0mm" }
+        } satisfies Record<string, DistanceData>;
+
+        const it = test.extend<{ row: EventRow }>({
+          row: async ({ task: _ }, use) => {
+            const mockDataWithRouteInfo = mock<calendar_v3.Schema$Event>({
+              start: { dateTime: start },
+              end: { dateTime: end },
+              location,
+              extendedProperties: {
+                private: {
+                  distanceInfo: JSON.stringify(mockDistanceData)
+                }
+              }
+            });
+            const emailGig = EmailGig.make(updatedLocation, start, end);
+            const calendarGig = GoogleGig.make(mockDataWithRouteInfo);
+
+            const row = EventRow.buildRow(emailGig, calendarGig, distanceService);
+            expect(row).instanceof(EventRow);
+            return await use(row);
+          }
+        });
+
+        it("is a FullCalendarGig", ({ row: { appGig } }) => {
+          expect(appGig).instanceof(FullCalendarGig);
+        });
+
+        it("has basic info that matches the email gig", ({ row: { appGig } }) => {
+          expect(appGig.getLocation()).toEqual(updatedLocation);
+          expect(appGig.getStartTime().dateTime).toEqual(start);
+          expect(appGig.getEndTime().dateTime).toEqual(end);
+          expect(appGig.getId()).toEqual("2024-12-01");
+        });
+
+        it("keeps the distance info empty, because it's out of date", ({ row: { appGig } }) => {
+          expect(distanceService.getDistanceInfo).not.toHaveBeenCalled();
+          expect(appGig.getRouteInfo()).toBeNull()
+        });
+
+        it("can fill out the route info on demand", async ({ row: { appGig } }) => {
+          expect(distanceService.getDistanceInfo).not.toHaveBeenCalled();
+          await appGig.fetchRouteInfo();
+          expect(distanceService.getDistanceInfo).toHaveBeenCalled();
+          expect(appGig.getRouteInfo()).not.toBeNull();
+        });
+      });
+
     });
   });
 });
