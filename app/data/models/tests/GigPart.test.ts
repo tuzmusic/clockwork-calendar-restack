d37776edeb180efd43bc8393dd1bfb342ba596b3
Jonathan Tuzman <jonathan.tuzman@butcherbox.com>
2024-05-06 20:00:10 -0400

    tests for reception and cocktail hour

--- app/data/models/tests/GigPart.test.ts
+++ app/data/models/tests/GigPart.test.ts
@@ -4,12 +4,42 @@ import { end, start } from "~/data/models/tests/testConstants";
 class GigPartImpl extends GigPart {
 }
 
-describe("GigPart", () => {
-  const part = new GigPartImpl("reception", start, end);
+class Reception extends GigPart {
+  constructor(startDateTime: string, endDateTime: string) {
+    super("reception", startDateTime, endDateTime);
+  }
+}
+class CocktailHour extends GigPart {
+  constructor(startDateTime: string, endDateTime: string) {
+    super("cocktail hour", startDateTime, endDateTime);
+  }
+}
 
-  test("type", () => expect(part.type).toEqual("reception"));
-  test("start", () => expect(part.startDateTime).toEqual(start));
-  test("end", () => expect(part.endDateTime).toEqual(end));
-  test("actualStart is null by default", () => expect(part.actualStart).toBeNull());
-  test("actualEnd is null by default", () => expect(part.actualEnd).toBeNull());
+describe("GigPart", () => {
+  describe("base class", () => {
+    const part = new GigPartImpl("reception", start, end);
+    test("type", () => expect(part.type).toEqual("reception"));
+    test("start", () => expect(part.startDateTime).toEqual(start));
+    test("end", () => expect(part.endDateTime).toEqual(end));
+    test("actualStart is null by default", () => expect(part.actualStart).toBeNull());
+    test("actualEnd is null by default", () => expect(part.actualEnd).toBeNull());
+  });
+
+  describe("Reception", () => {
+    const part = new Reception(start, end);
+    test("type", () => expect(part.type).toEqual("reception"));
+    test("start", () => expect(part.startDateTime).toEqual(start));
+    test("end", () => expect(part.endDateTime).toEqual(end));
+    test("actualStart is null", () => expect(part.actualStart).toBeNull());
+    test("actualEnd is null", () => expect(part.actualEnd).toBeNull());
+  });
+
+  describe("Cocktail Hour", () => {
+    const part = new CocktailHour(start, end);
+    test("type", () => expect(part.type).toEqual("cocktail hour"));
+    test("start", () => expect(part.startDateTime).toEqual(start));
+    test("end", () => expect(part.endDateTime).toEqual(end));
+    test("actualStart is null", () => expect(part.actualStart).toBeNull());
+    test("actualEnd is null", () => expect(part.actualEnd).toBeNull());
+  });
 });
