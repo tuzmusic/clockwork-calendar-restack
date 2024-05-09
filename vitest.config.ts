975f60b612cac013b40a23f19e9890088894a899
Jonathan Tuzman <jonathan.tuzman@butcherbox.com>
2024-05-09 16:26:27 -0400

    configure vitest TZ

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,5 +11,6 @@ export default defineConfig({
     globals: true,
     environment: "happy-dom",
     setupFiles: ["./test/setup-test-env.ts"],
-  },
+    globalSetup: "./vitest.global-setup.ts"
+  }
 });
