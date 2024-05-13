f45970990d924ec159736565cee04a69609dbaae
Jonathan Tuzman <jonathan.tuzman@butcherbox.com>
2024-05-13 11:09:29 -0400

    failing test for SimpleGig

---
+++ app/data/models/tests/SimpleGig.test.ts
@@ -0,0 +1,32 @@
+const laterTime = "2024-12-01T19:00:00-04:00";
+const earlierTime = "2024-12-01T18:00:00-04:00";
+const finalTime = "2024-12-01T21:00:00-04:00";
+
+describe("GigWithParts abstract class", () => {
+  class simpleGigImpl extends SimpleGig {
+    public static make(location: string, startDateTime: string, endDateTime: string) {
+      return new this(location, startDateTime, endDateTime);
+    }
+  }
+
+  describe("public data", () => {
+    const gig = simpleGigImpl.make("somewhere", earlierTime, finalTime);
+
+    it("has a location", () => {
+      expect(gig.getLocation()).toEqual("somewhere");
+    });
+
+    it("has a start time", () => {
+      expect(gig.getStartTime()).toEqual(earlierTime);
+    });
+
+    it("has an end time", () => {
+      expect(gig.getEndTime()).toEqual(finalTime);
+    });
+
+    it("has an id based on its start date", () => {
+      expect(gig.getId()).toEqual("2024-12-01");
+    });
+  });
+});
+
