2af3551f989ed580ac46a579a957bee5479177ff
Jonathan Tuzman <jonathan.tuzman@butcherbox.com>
2024-05-14 16:26:25 -0400

    pass using whitespace-ignore utility

--- app/data/parsers/emailParser/tests/parseEventParts.test.ts
+++ app/data/parsers/emailParser/tests/parseEventParts.test.ts
@@ -3,157 +3,168 @@ import EmailParser from "~/data/parsers/emailParser/EmailParser";
 
 import { buildEvent, buildHtml, buildMonthHeader, buildOtherPart } from "./htmlBuilders";
 
-const location = 'Lenox Hotel, Boston, MA'
+const location = "Lenox Hotel, Boston, MA";
 
-describe('Parsing event parts', () => {
-  describe('Reception', () => {
-    it('Parses an event with reception only', () => {
+function assertEqualsIgnoringWhitespace(str1: string, str2: string) {
+  expect(str1.replace(/\s+/g, ``)).toEqual(str2.replace(/\s+/g, ``));
+}
+
+describe("Parsing event parts", () => {
+  describe("Reception", () => {
+    it("Parses an event with reception only", () => {
       const eventHtml = buildEvent({
         dateNum: 8,
-        timeStr: '6:00-10:30',
-        location,
+        timeStr: "6:00-10:30",
+        location
       });
       const html = buildHtml(
-        buildMonthHeader('July, 2024'),
+        buildMonthHeader("July, 2024"),
         eventHtml
-      )
+      );
       const gigs = EmailParser.parseEmail(html);
-      const event = gigs.shift()!
+      const event = gigs.shift()!;
 
-      expect(event.getParts()).toHaveLength(1)
-      const [reception] = event.getParts()
+      expect(event.getParts()).toHaveLength(1);
+      const [reception] = event.getParts();
       testEventPart(
         reception,
-        'reception',
-        '2024-07-08T18:00:00-04:00',
-        '2024-07-08T22:30:00-04:00'
-      )
+        "reception",
+        "2024-07-08T18:00:00-04:00",
+        "2024-07-08T22:30:00-04:00"
+      );
 
-      expect(event.getOriginalHtml()).toEqual(eventHtml.trim())
-    })
+      assertEqualsIgnoringWhitespace(
+        event.getOriginalHtml() ?? "",
+        eventHtml.trim()
+      );
+    });
 
-    it('Parses an event with reception ending after 12am (but before 1am)', () => {
+    it("Parses an event with reception ending after 12am (but before 1am)", () => {
       const html = buildHtml(
-        buildMonthHeader('July, 2024'),
+        buildMonthHeader("July, 2024"),
         buildEvent({
           dateNum: 8,
-          timeStr: '6:00-12:30',
-          location,
+          timeStr: "6:00-12:30",
+          location
         })
-      )
-      const event = EmailParser.parseEmail(html).shift()!
+      );
+      const event = EmailParser.parseEmail(html).shift()!;
 
-      expect(event.getParts()).toHaveLength(1)
-      const [reception] = event.getParts()
+      expect(event.getParts()).toHaveLength(1);
+      const [reception] = event.getParts();
       testEventPart(
         reception,
-        'reception',
-        '2024-07-08T18:00:00-04:00',
-        '2024-07-09T00:30:00-04:00'
-      )
-      expect(event.getId()).toEqual('2024-07-08')
-    })
+        "reception",
+        "2024-07-08T18:00:00-04:00",
+        "2024-07-09T00:30:00-04:00"
+      );
+      expect(event.getId()).toEqual("2024-07-08");
+    });
 
-    it('Parses an event with reception ending AT 12am', () => {
+    it("Parses an event with reception ending AT 12am", () => {
       const html = buildHtml(
-        buildMonthHeader('July, 2024'),
+        buildMonthHeader("July, 2024"),
         buildEvent({
           dateNum: 8,
-          timeStr: '6:00-12:00',
-          location,
+          timeStr: "6:00-12:00",
+          location
         })
-      )
-      const event = EmailParser.parseEmail(html).shift()!
+      );
+      const event = EmailParser.parseEmail(html).shift()!;
 
-      expect(event.getParts()).toHaveLength(1)
-      const [reception] = event.getParts()
+      expect(event.getParts()).toHaveLength(1);
+      const [reception] = event.getParts();
       testEventPart(
         reception,
-        'reception',
-        '2024-07-08T18:00:00-04:00',
-        '2024-07-09T00:00:00-04:00'
-      )
-    })
-  })
+        "reception",
+        "2024-07-08T18:00:00-04:00",
+        "2024-07-09T00:00:00-04:00"
+      );
+    });
+  });
 
-  describe('Cocktail hour and ceremony', () => {
-    it('Parses an event with a reception and cocktail hour', () => {
+  describe("Cocktail hour and ceremony", () => {
+    it("Parses an event with a reception and cocktail hour", () => {
       const receptionStr = buildEvent({
         dateNum: 8,
-        timeStr: '6:00-10:30',
-        location,
+        timeStr: "6:00-10:30",
+        location
       });
-      const cocktailStr = buildOtherPart({ timeStr: '5:00-6:00', part: 'Cocktail Hour' });
+      const cocktailStr = buildOtherPart({ timeStr: "5:00-6:00", part: "Cocktail Hour" });
 
       const html = buildHtml(
-        buildMonthHeader('July, 2024'),
+        buildMonthHeader("July, 2024"),
         receptionStr,
         cocktailStr
-      )
+      );
 
-      const event = EmailParser.parseEmail(html).shift()!
+      const event = EmailParser.parseEmail(html).shift()!;
 
-      expect(event.getOriginalHtml()).toEqual(`${receptionStr.trim()}${cocktailStr.trim()}`)
-      expect(event.getParts()).toHaveLength(2)
-      const [cocktails, reception] = event.getParts() // implicitly tests ordering
+      assertEqualsIgnoringWhitespace(
+        event.getOriginalHtml() ?? "",
+        `${receptionStr.trim()}${cocktailStr.trim()}`
+      );
+      // expect(event.getOriginalHtml()).toEqual(`${receptionStr.trim()}${cocktailStr.trim()}`);
+      expect(event.getParts()).toHaveLength(2);
+      const [cocktails, reception] = event.getParts(); // implicitly tests ordering
       testEventPart(
         reception,
-        'reception',
-        '2024-07-08T18:00:00-04:00',
-        '2024-07-08T22:30:00-04:00'
-      )
+        "reception",
+        "2024-07-08T18:00:00-04:00",
+        "2024-07-08T22:30:00-04:00"
+      );
 
       testEventPart(
         cocktails,
-        'cocktail hour',
-        '2024-07-08T17:00:00-04:00',
-        '2024-07-08T18:00:00-04:00'
-      )
-    })
+        "cocktail hour",
+        "2024-07-08T17:00:00-04:00",
+        "2024-07-08T18:00:00-04:00"
+      );
+    });
 
-    it('Parses an event with a reception, ceremony and cocktail hour', () => {
+    it("Parses an event with a reception, ceremony and cocktail hour", () => {
       const html = buildHtml(
-        buildMonthHeader('July, 2024'),
+        buildMonthHeader("July, 2024"),
         buildEvent({
           dateNum: 8,
-          timeStr: '6:00-10:30',
-          location,
+          timeStr: "6:00-10:30",
+          location
         }),
-        buildOtherPart({ timeStr: '4:30-5:00', part: 'Ceremony' }),
-        buildOtherPart({ timeStr: '5:00-6:00', part: 'Cocktail Hour' })
-      )
+        buildOtherPart({ timeStr: "4:30-5:00", part: "Ceremony" }),
+        buildOtherPart({ timeStr: "5:00-6:00", part: "Cocktail Hour" })
+      );
 
-      const event = EmailParser.parseEmail(html).shift()!
+      const event = EmailParser.parseEmail(html).shift()!;
 
-      expect(event.getParts()).toHaveLength(3)
-      const [ceremony, cocktails, reception] = event.getParts()
+      expect(event.getParts()).toHaveLength(3);
+      const [ceremony, cocktails, reception] = event.getParts();
       testEventPart(
         reception,
-        'reception',
-        '2024-07-08T18:00:00-04:00',
-        '2024-07-08T22:30:00-04:00'
-      )
+        "reception",
+        "2024-07-08T18:00:00-04:00",
+        "2024-07-08T22:30:00-04:00"
+      );
 
       testEventPart(
         cocktails,
-        'cocktail hour',
-        '2024-07-08T17:00:00-04:00',
-        '2024-07-08T18:00:00-04:00'
-      )
+        "cocktail hour",
+        "2024-07-08T17:00:00-04:00",
+        "2024-07-08T18:00:00-04:00"
+      );
 
       testEventPart(
         ceremony,
-        'ceremony',
-        '2024-07-08T16:30:00-04:00', // schedule says ceremony starts at 4:30
-        '2024-07-08T17:00:00-04:00'
-      )
+        "ceremony",
+        "2024-07-08T16:30:00-04:00", // schedule says ceremony starts at 4:30
+        "2024-07-08T17:00:00-04:00"
+      );
 
       expect(ceremony.actualStartDateTime).toEqual(
-        '2024-07-08T16:00:00-04:00' // but we start playing at 4:00
-      )
-    })
-  })
-})
+        "2024-07-08T16:00:00-04:00" // but we start playing at 4:00
+      );
+    });
+  });
+});
 
 function testEventPart(
   part: GigPart,
@@ -161,7 +172,7 @@ function testEventPart(
   startTime: string,
   endTime: string
 ) {
-  expect(part.type).toEqual(type)
-  expect(part.startDateTime).toEqual(startTime)
-  expect(part.endDateTime).toEqual(endTime)
+  expect(part.type).toEqual(type);
+  expect(part.startDateTime).toEqual(startTime);
+  expect(part.endDateTime).toEqual(endTime);
 }
