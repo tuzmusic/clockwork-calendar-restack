3d5df9fdeb47a984a5f00f6e87568816e0dbc77f
Jonathan Tuzman <jonathan.tuzman@butcherbox.com>
2024-05-15 17:11:19 -0400

    tsconfig paths plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 installGlobals()
 
@@ -8,5 +9,5 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [remix()],
+  plugins: [remix(), tsconfigPaths()],
 });
