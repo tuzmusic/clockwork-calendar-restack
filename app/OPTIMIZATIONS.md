5b92814724fa3c9aa6e155bc1ce9dacd12bc7539
Jonathan Tuzman <jonathan.tuzman@butcherbox.com>
2024-05-16 11:12:12 -0400

    add month to date in email html

--- app/OPTIMIZATIONS.md
+++ app/OPTIMIZATIONS.md
@@ -1,9 +1,10 @@
 # Optimizations, Refinements, Refactoring
 
-- EventPart *class*
-  - Ceremony actualStart handled in subclass
-- EventTimeline(?) class, handles ordering events
-  - Also gets event start and end times?
+- [X] EventPart *class*
+  - [X] Ceremony actualStart handled in subclass
+- [X] EventTimeline(?) class, handles ordering events
+  - [X] Also gets event start and end times?
 - Location class, handles distances and stuff?
 - Use start time when getting route info
-- 
+- Better width/alignment in email html 
+  - probably best to just construct it from data, with grid layout, and scrap the original html
--- app/routes/events/components/EmailHtml.tsx
+++ app/routes/events/components/EmailHtml.tsx
@@ -1,11 +1,21 @@
+import DayJsTz from "~/data/models/DayJsTz";
 import EmailGig from "~/data/models/EmailGig";
 
 export function EmailHtml({ gig }: { gig: ReturnType<EmailGig["serialize"]> }) {
+  // get the gig month and insert it into the <td> tag with the date
+  const month = DayJsTz(gig.id).format("MMM");
+  // replace the month in the first <td> tag of the originalHtml with the month
+  const html = gig.originalHtml?.replace(
+    /<td><strong>(\d{1,2})/,
+    `<td class="text-right"><strong>${month} $1`
+  );
+
+
   return (
-    <table>
+    <table cellPadding="2" cellSpacing="2">
       <tbody
         className="align-top"
-        dangerouslySetInnerHTML={{ __html: gig.originalHtml ?? "(email html here)" }}
+        dangerouslySetInnerHTML={{ __html: html ?? "(email html here)" }}
       />
     </table>
   );
