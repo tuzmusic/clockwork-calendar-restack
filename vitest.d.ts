5a46abc852125c8b0f7d3e748ba6200068f42bf0
Jonathan Tuzman <jonathan.tuzman@butcherbox.com>
2024-05-16 17:18:49 -0400

    delete vitest.d.ts because it's redeclaring instead of extending?

--- vitest.d.ts
+++
@@ -1,8 +0,0 @@
-interface CustomMatchers<R = unknown> {
-  toEqualIgnoringWhitespace: () => R
-}
-
-declare module 'vitest' {
-  type Assertion<T = any> = CustomMatchers<T>
-  interface AsymmetricMatchersContaining extends CustomMatchers {}
-}
