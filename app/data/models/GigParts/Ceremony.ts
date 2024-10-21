1c9e63d560244cd231e1550f82edefb743310151
Jonathan Tuzman <jonathan.tuzman@butcherbox.com>
2024-10-21 16:23:56 -0400

    pass one test, fail another...

--- app/data/models/GigParts/Ceremony.ts
+++ app/data/models/GigParts/Ceremony.ts
@@ -1,13 +1,21 @@
-import { TIME_ZONE } from "~/data/models/constants";
-import DayJsTz from "~/data/models/DayJsTz";
+import dayjs from "dayjs";
+
 import { GigPart } from "~/data/models/GigParts/GigPart";
 
 export class Ceremony extends GigPart {
   constructor(startDateTime: string, endDateTime: string) {
     super("ceremony", startDateTime, endDateTime);
 
-    const writtenStartDay = DayJsTz(startDateTime);
+    // const writtenStartDay = DayJsTz(startDateTime);
+    const writtenStartDay = dayjs(startDateTime);
     const actualStartDay = writtenStartDay.subtract(30, "minutes");
-    this.actualStartDateTime = actualStartDay.tz(TIME_ZONE).format();
+
+    const actualStartDateTime = actualStartDay//.tz(TIME_ZONE);
+
+    const actualStartDateTimeFormatted = actualStartDateTime.format()
+      // remove time zone suffix
+      .replace(/(:00)-0.*/, "$1");
+
+    this.actualStartDateTime = actualStartDateTimeFormatted;
   }
 }
