59527b4876478b5540e5c0da5df3b9a881117755
Jonathan Tuzman <jonathan.tuzman@butcherbox.com>
2025-06-08 13:15:49 -0400

    move event row ui to its own file. but, responsive ui isn't actually working correctly

---
+++ app/routes/events/components/EventRowUI.tsx
@@ -0,0 +1,31 @@
+import React, { ComponentProps } from "react";
+
+import { EventRowJson } from "~/data/models/EventRow";
+import { CalendarGigUI } from "~/routes/events/components/CalendarGigUI";
+import { EmailHtml } from "~/routes/events/components/EmailHtml";
+import { FullGigUI } from "~/routes/events/components/FullGigUI";
+import { RoundedWrapper } from "~/routes/events/components/RoundedWrapper";
+import { SaveGigButton } from "~/routes/events/components/SaveGigButton";
+
+const MobileWrapper = (props: ComponentProps<typeof RoundedWrapper>) =>
+  <RoundedWrapper className={`hidden sm:visible ${props.className ?? ""}`}>
+    {props.children}
+  </RoundedWrapper>;
+
+export function EventRowUI({ row }: { row: EventRowJson }) {
+  return <React.Fragment key={row.id}>
+    <MobileWrapper className={"bg-amber-500 sm:bg-amber-200"}>
+      {row.emailGig ? <EmailHtml gig={row.emailGig} /> : null}
+    </MobileWrapper>
+
+    <RoundedWrapper>
+      <FullGigUI row={row} />
+    </RoundedWrapper>
+
+    <MobileWrapper className={"bg-blue-200 sm:bg-blue-600"}>
+      {row.googleGig
+        ? <CalendarGigUI row={row} hasUpdates={row.hasUpdates} />
+        : <SaveGigButton row={row} />}
+    </MobileWrapper>
+  </React.Fragment>;
+}
--- app/routes/events/components/EventsTable.tsx
+++ app/routes/events/components/EventsTable.tsx
@@ -1,11 +1,5 @@
-import React, { ComponentProps } from "react";
-
 import { EventRowJson } from "~/data/models/EventRow";
-import { CalendarGigUI } from "~/routes/events/components/CalendarGigUI";
-import { EmailHtml } from "~/routes/events/components/EmailHtml";
-import { FullGigUI } from "~/routes/events/components/FullGigUI";
-import { RoundedWrapper } from "~/routes/events/components/RoundedWrapper";
-import { SaveGigButton } from "~/routes/events/components/SaveGigButton";
+import { EventRowUI } from "~/routes/events/components/EventRowUI";
 
 export function EventsTable({ eventRows }: { eventRows: EventRowJson[] }) {
   return (
@@ -19,30 +13,7 @@ export function EventsTable({ eventRows }: { eventRows: EventRowJson[] }) {
         <h2 className="text-right">Calendar</h2>
       </div>
 
-      {eventRows.map((row) => {
-          const MobileWrapper = (props: ComponentProps<typeof RoundedWrapper>) =>
-            <RoundedWrapper className={`hidden sm:visible ${props.className ?? ""}`}>
-              {props.children}
-            </RoundedWrapper>;
-
-          return <React.Fragment key={row.id}>
-            <MobileWrapper className={"bg-amber-500 sm:bg-amber-200"}>
-              {row.emailGig ? <EmailHtml gig={row.emailGig} /> : null}
-            </MobileWrapper>
-
-            <RoundedWrapper>
-              <FullGigUI row={row} />
-            </RoundedWrapper>
-
-            <MobileWrapper className={"bg-blue-200 sm:bg-blue-600"}>
-              {row.googleGig
-                ? <CalendarGigUI row={row} hasUpdates={row.hasUpdates} />
-                : <SaveGigButton row={row} />}
-            </MobileWrapper>
-          </React.Fragment>;
-        }
-      )}
-
+      {eventRows.map((row) => <EventRowUI row={row} key={row.id} />)}
     </div>
   );
 }
