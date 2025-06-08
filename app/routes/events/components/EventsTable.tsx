d114ac4fdbda75b99b31610d1053f608690f8dec
Jonathan Tuzman <jonathan.tuzman@butcherbox.com>
2025-06-08 13:09:03 -0400

    show only Full Gig in mobile

--- app/routes/events/components/EventsTable.tsx
+++ app/routes/events/components/EventsTable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ComponentProps } from "react";
 
 import { EventRowJson } from "~/data/models/EventRow";
 import { CalendarGigUI } from "~/routes/events/components/CalendarGigUI";
@@ -9,9 +9,9 @@ import { SaveGigButton } from "~/routes/events/components/SaveGigButton";
 
 export function EventsTable({ eventRows }: { eventRows: EventRowJson[] }) {
   return (
-    <div data-testid="EVENTS_TABLE" className="grid grid-cols-3 items-start gap-3">
+    <div data-testid="EVENTS_TABLE" className="grid sm:grid-cols-3 items-start gap-3">
       <div className={
-        "col-span-3 grid grid-cols-3 gap-3 font-bold "
+        "hidden sm:grid col-span-3 grid-cols-3 gap-3 font-bold "
         + "*:bg-gray-200 *:w-full *:p-2"
       }>
         <h2>Email</h2>
@@ -19,23 +19,30 @@ export function EventsTable({ eventRows }: { eventRows: EventRowJson[] }) {
         <h2 className="text-right">Calendar</h2>
       </div>
 
-      {eventRows.map((row) =>
-        <React.Fragment key={row.id}>
-          <RoundedWrapper>
-            {row.emailGig ? <EmailHtml gig={row.emailGig} /> : null}
-          </RoundedWrapper>
+      {eventRows.map((row) => {
+          const MobileWrapper = (props: ComponentProps<typeof RoundedWrapper>) =>
+            <RoundedWrapper className={`hidden sm:visible ${props.className ?? ""}`}>
+              {props.children}
+            </RoundedWrapper>;
 
-          <RoundedWrapper>
-            <FullGigUI row={row} />
-          </RoundedWrapper>
+          return <React.Fragment key={row.id}>
+            <MobileWrapper className={"bg-amber-500 sm:bg-amber-200"}>
+              {row.emailGig ? <EmailHtml gig={row.emailGig} /> : null}
+            </MobileWrapper>
 
-          <RoundedWrapper>
-            {row.googleGig
-              ? <CalendarGigUI row={row} hasUpdates={row.hasUpdates} />
-              : <SaveGigButton row={row} />}
-          </RoundedWrapper>
-        </React.Fragment>
+            <RoundedWrapper>
+              <FullGigUI row={row} />
+            </RoundedWrapper>
+
+            <MobileWrapper className={"bg-blue-200 sm:bg-blue-600"}>
+              {row.googleGig
+                ? <CalendarGigUI row={row} hasUpdates={row.hasUpdates} />
+                : <SaveGigButton row={row} />}
+            </MobileWrapper>
+          </React.Fragment>;
+        }
       )}
+
     </div>
   );
 }
