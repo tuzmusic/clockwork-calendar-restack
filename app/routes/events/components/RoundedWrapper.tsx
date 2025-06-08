be36a7013f27c5566e691f305e9ca90b4db4f11b
Jonathan Tuzman <jonathan.tuzman@butcherbox.com>
2025-06-08 13:08:53 -0400

    RoundedWrapper accepts more classNames

--- app/routes/events/components/RoundedWrapper.tsx
+++ app/routes/events/components/RoundedWrapper.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from "react";
 
-export function RoundedWrapper(props: PropsWithChildren) {
-  return <div className="border-gray-500 border rounded-md h-full" {...props} />;
+export function RoundedWrapper({ className = "", ...props }: PropsWithChildren & { className?: string }) {
+  return <div className={`border-gray-500 border rounded-md h-full ${className}`} {...props} />;
 }
