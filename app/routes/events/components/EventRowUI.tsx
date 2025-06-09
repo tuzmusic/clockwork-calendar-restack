47553c0764e11d917cccb10a394c1f0eb489df79
Jonathan Tuzman <jonathan.tuzman@butcherbox.com>
2025-06-09 13:47:02 -0400

    basic tab UI

--- app/routes/events/components/EventRowUI.tsx
+++ app/routes/events/components/EventRowUI.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentProps } from "react";
+import React, { ComponentProps, useState } from "react";
 
 import { EventRowJson } from "~/data/models/EventRow";
 import { CalendarGigUI } from "~/routes/events/components/CalendarGigUI";
@@ -13,14 +13,27 @@ const MobileWrapper = (props: ComponentProps<typeof RoundedWrapper>) =>
   </RoundedWrapper>;
 
 export function EventRowUI({ row }: { row: EventRowJson }) {
+  const tabs = ["Email", "Full", "Calendar"];
+  const [selectedTab, setSelectedTab] = useState<(typeof tabs)[number]>("Full");
   return <React.Fragment key={row.id}>
     <MobileWrapper className={"bg-amber-500 sm:bg-amber-200"}>
       {row.emailGig ? <EmailHtml gig={row.emailGig} /> : null}
     </MobileWrapper>
 
-    <RoundedWrapper>
-      <FullGigUI row={row} />
-    </RoundedWrapper>
+    <div>
+      <RoundedWrapper>
+        <FullGigUI row={row} />
+      </RoundedWrapper>
+      <div className="flex">
+        {tabs.map(name => (
+          <Tab name={name}
+               selected={selectedTab === name}
+               key={name}
+               onSelect={() => setSelectedTab(name)}
+          />
+        ))}
+      </div>
+    </div>
 
     <MobileWrapper className={"bg-blue-600 sm:bg-blue-200"}>
       {row.googleGig
@@ -28,4 +41,13 @@ export function EventRowUI({ row }: { row: EventRowJson }) {
         : <SaveGigButton row={row} />}
     </MobileWrapper>
   </React.Fragment>;
+}
+
+function Tab(props: { name: string, selected: boolean, onSelect: (name: string) => void }) {
+  return <button type="button"
+                 onClick={() => props.onSelect(props.name)}
+                 className={`${props.selected ? "bg-gray-500" : "bg-gray-300"} p-2`}
+  >
+    {props.name}
+  </button>;
 }
