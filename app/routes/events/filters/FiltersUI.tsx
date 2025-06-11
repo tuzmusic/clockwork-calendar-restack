fe2d9b0895d7bf993608751d2ebce42b907f84d1
Jonathan Tuzman <jonathan.tuzman@butcherbox.com>
2025-06-11 10:16:16 -0400

    improve label/checkbox aXXe

--- app/routes/events/filters/FiltersUI.tsx
+++ app/routes/events/filters/FiltersUI.tsx
@@ -6,17 +6,19 @@ export function FiltersUI({ filters, toggleFilter }: {
 }) {
   return (
     <div className="flex gap-2">
-      <h3> Filter: </h3>
+      <h3>Filter:</h3>
       <ul>
         {FILTER_NAMES.map((filterName) => (
-          <li className="flex gap-1" key={filterName}>
-            <input
-              id={filterName}
-              type="checkbox"
-              checked={filters.includes(filterName)}
-              onChange={() => toggleFilter(filterName)}
-            />
-            <label>{filterName}</label>
+          <li key={filterName}>
+            <label className="flex gap-1">
+              <input
+                id={filterName}
+                type="checkbox"
+                checked={filters.includes(filterName)}
+                onChange={() => toggleFilter(filterName)}
+              />
+              <span>{filterName}</span>
+            </label>
           </li>
         ))}
       </ul>
