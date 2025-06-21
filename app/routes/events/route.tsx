fb8973328174f2a070cafbc78676e7407a755e3d
Jonathan Tuzman <jonathan.tuzman@butcherbox.com>
2025-06-21 13:10:26 -0400

    remove old distanceInfo code from route

--- app/routes/events/route.tsx
+++ app/routes/events/route.tsx
@@ -1,7 +1,6 @@
-import { useActionData, useFetchers, useLoaderData } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 
 import { action } from "~/routes/events/eventsAction.server";
-import { EventsActionIntent } from "~/routes/events/EventsActionIntent";
 import { EventsPage } from "~/routes/events/page/EventsPage";
 
 import { loader } from "./eventsLoader.server";
@@ -12,20 +11,6 @@ export { action, loader };
 
 export default function EventsRoute() {
   const { eventRowsJson } = useLoaderData<typeof loader>();
-  const actionData = useActionData<typeof action>();
-
-  const fetchers = useFetchers()
-  console.log(fetchers);
-
-  switch (actionData?.intent) {
-    case EventsActionIntent.getDistanceInfo: {
-      const updatedRow = eventRowsJson.find(row => row.id === actionData.id);
-      if (updatedRow) {
-        updatedRow.appGig.distanceInfo = actionData.distanceInfo;
-        updatedRow.hasUpdates = true;
-      }
-    }
-  }
 
   return <EventsPage eventRows={eventRowsJson} />;
 };
