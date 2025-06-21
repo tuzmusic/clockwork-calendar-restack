613bdf35d95c38d52984a0066ea6e3a26ee128d5
Jonathan Tuzman <jonathan.tuzman@butcherbox.com>
2025-06-21 14:03:30 -0400

    implement simple "alwaysShow" filter

--- app/routes/events/filters/useEventFilters.ts
+++ app/routes/events/filters/useEventFilters.ts
@@ -1,14 +1,14 @@
 import { useSearchParams } from "@remix-run/react";
+import { useCallback } from "react";
 
 import { EventRowJson } from "~/data/models/EventRow";
 
 import { AvailableFilter, FILTERS } from "./filters";
 
-export function useEventFilters(rows: EventRowJson[]) {
-  const [params, setParams] = useSearchParams();
-  const filters = params.getAll("filter") as AvailableFilter[];
 
-  const toggleFilter = (key: AvailableFilter) => {
+const useToggleFilter = () => {
+  const [params, setParams] = useSearchParams();
+  return useCallback((key: AvailableFilter) => {
     const currentFilters = params.getAll("filter");
     const hasFilter = currentFilters.includes(key);
 
@@ -21,12 +21,23 @@ export function useEventFilters(rows: EventRowJson[]) {
     newFilters.forEach(f => newParams.append("filter", f)); // re-add filtered ones
 
     setParams(newParams);
-  };
+  }, [params, setParams]);
+};
+
+export function useEventFilters(rows: EventRowJson[]) {
+  const [params] = useSearchParams();
+  const toggleFilter = useToggleFilter()
+
+  const filters = params.getAll("filter") as AvailableFilter[];
+  const alwaysShow = params.getAll("alwaysShow");
 
   const filteredEvents = !filters.length
     ? rows
     : rows?.filter(row =>
-      filters.reduce((yet, filter) => yet && FILTERS[filter as AvailableFilter](row, rows), true)
+      filters.reduce((yet, filter) => {
+        if (alwaysShow.includes(row.id)) return true;
+        return yet && FILTERS[filter as AvailableFilter](row, rows);
+      }, true)
     );
 
   return { toggleFilter, filteredEvents, filters };
