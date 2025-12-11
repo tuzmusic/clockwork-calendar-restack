a502d0cf1a45b059ca357f9b8705a7a03232f420
Jonathan Tuzman <tuzmusic@gmail.com>
2025-12-11 11:45:29 -0500

    use clerk for signin (But doesn't go past yet)

--- app/routes/login/route.tsx
+++ app/routes/login/route.tsx
@@ -1,3 +1,10 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+  UserButton,
+} from "@clerk/remix";
 import { LoaderFunction, redirect } from "@remix-run/node";
 
 import { oauth2Client } from "~/auth/auth0.server";
@@ -5,12 +12,12 @@ import { oauth2Client } from "~/auth/auth0.server";
 export const action: LoaderFunction = async () => {
   const scopes = [
     "https://www.googleapis.com/auth/calendar",
-    "https://www.googleapis.com/auth/gmail.readonly"
+    "https://www.googleapis.com/auth/gmail.readonly",
   ];
 
   const authorizeUrl = oauth2Client.generateAuthUrl({
     access_type: "offline",
-    scope: scopes.join(" ")
+    scope: scopes.join(" "),
   });
 
   return redirect(authorizeUrl);
@@ -18,11 +25,29 @@ export const action: LoaderFunction = async () => {
 
 export default function LoginPage() {
   return (
-    <form method="post"
-          className={"flex flex-col w-full py-10 h-full gap-2 "
-            + "items-center justify-start sm:justify-center"}>
-      <button type="submit" className="border-2 p-2 h-min">Log In</button>
-      <a className={"hover:underline"} href={"/events?useFixture=true"}>Go to events page with fixture</a>
-    </form>
+    <div
+      className={
+        "flex flex-col w-full py-10 h-full gap-2 " +
+        "items-center justify-start sm:justify-center"
+      }
+    >
+      <SignedIn>
+        <div>
+          <UserButton />
+        </div>
+        <div className="border-2 p-2 h-min">
+          <SignOutButton />
+        </div>
+      </SignedIn>
+
+      <SignedOut>
+        <div className="border-2 p-2 h-min">
+          <SignInButton />
+        </div>
+        <a className={"hover:underline"} href={"/events?useFixture=true"}>
+          Go to events page with fixture
+        </a>
+      </SignedOut>
+    </div>
   );
 }
